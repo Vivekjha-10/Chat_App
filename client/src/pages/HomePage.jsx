19f40a517167ed2cb9af61a0ba03d79a4226a899
Vivekjha-10 <jhavivekkumar81@gmail.com>
2026-02-13 07:55:45 +0530

    chat conterner done

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -12,8 +12,8 @@ const HomePage = () => {
         className={`grid backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] relative 
         ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}
       >
-        <Sidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
-        <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+        <Sidebar />
+        <ChatContainer/>
         <RightSidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
       </div>
     </div>
--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -55,7 +55,7 @@ export const getMessages = async (req, res) => {
 
 
 // api to mark message as seen using message id 
-export const markMessageAsSeen = async () => {
+export const markMessageAsSeen = async (req, res) => {
     try {
         const { id } = req.params;
         await Message.findByIdAndUpdate(id, { seen: true })
